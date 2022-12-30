1c7b472306b55129e5075a4ecc3f940e32a42008
Akash Chaurasiya <akashchaurasia617@gmail.com>
2022-12-30 12:31:50 +0530

    first project webservices restfull api
    my first project webservices restfull api

---
+++ A first-spring-boot/my-dev/app1/app1/src/main/java/com/akash/app1/App1Application.java
@@ -0,0 +1,14 @@
+package com.akash.app1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App1Application.class, args);
+		System.out.println("my first spring boot project...");
+	}
+
+}
---
+++ A first-spring-boot/my-dev/app3/app3/src/test/java/com/akash/app3/App3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akash.app3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
